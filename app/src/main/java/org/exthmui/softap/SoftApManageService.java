3cbf72ccfc5d0ee7d1aa83cc449e2068b4f8ea5a
cjybyjk <cjybyjk@zjnu.edu.cn>
2021-10-03 03:33:37 +0800

    use TetheringManager for getting client info
    In the previous code, the IP address of the client is obtained by executing the command
    This will make it difficult to determine whether the client is reachable and obtain the host name
    This process can be optimized by using TetheringManager
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Ic35735b001bd9f41baf663a86fbeffc761fedeaf

--- app/src/main/java/org/exthmui/softap/SoftApManageService.java
+++ app/src/main/java/org/exthmui/softap/SoftApManageService.java
@@ -1,23 +1,19 @@
 package org.exthmui.softap;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.WifiManager;
-import android.net.wifi.WifiClient;
+import android.net.TetheringManager;
+import android.net.TetheredClient;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.HandlerExecutor;
 import android.os.IBinder;
 import android.os.INetworkManagementService;
-import android.os.Message;
+import android.os.Looper;
 import android.os.ServiceManager;
 import android.text.TextUtils;
 import android.util.Log;
-
-import androidx.annotation.NonNull;
 
 import org.exthmui.softap.model.ClientInfo;
 import org.exthmui.softap.oui.MACData;
@@ -29,23 +25,11 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.HashMap;
 
-import static android.net.wifi.WifiManager.EXTRA_WIFI_AP_INTERFACE_NAME;
-
-public class SoftApManageService extends Service implements WifiManager.SoftApCallback {
-
-    private static final String IP_NEIGHBOR_COMMAND_FORMAT = "ip neigh show dev %s";
-    private static final int COLUMN_IP_ADDRESS = 0;
-    private static final int COLUMN_MAC_ADDRESS = 2;
-
-    private static final int MSG_STATUS_CHANGED = 1;
-    private static final int MSG_UPDATE_CLIENT_LIST = 2;
-
+public class SoftApManageService extends Service implements TetheringManager.TetheringEventCallback {
     public static final int STATUS_NORMAL = 0;
     public static final int STATUS_CLIENTS_REFRESHING = 1;
     public static final int STATUS_CLIENTS_REFRESH_DONE = 2;
@@ -55,44 +39,15 @@ public class SoftApManageService extends Service implements WifiManager.SoftApCa
     private static File BLOCKED_MAC_ADDRESS_FILE;
 
     private INetworkManagementService mNetworkManagementService;
-    private WifiManager mWifiManager;
+    private TetheringManager mTetheringManager;
 
-    private String mInterfaceName;
     private final ArrayList<String> mBlockedMACList = new ArrayList<>();
     private final ArrayList<StatusListener> mListeners = new ArrayList<>();
     private final HashMap<String, ClientInfo> mClients = new HashMap<>();
 
-    private Thread mClientUpdateThread;
     private Handler mHandler;
 
-    /* Use this receiver to get interface name and register callback */
-    private final BroadcastReceiver mApStatusReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (WifiManager.WIFI_AP_STATE_CHANGED_ACTION.equals(intent.getAction())) {
-                mInterfaceName = intent.getStringExtra(EXTRA_WIFI_AP_INTERFACE_NAME);
-                unregisterReceiver(this);
-                mWifiManager.registerSoftApCallback(new HandlerExecutor(mHandler),SoftApManageService.this);
-            }
-        }
-    };
-
     public SoftApManageService() {
-    }
-
-    private Thread createClientUpdateThread(final List<WifiClient> clients) {
-        return new Thread() {
-            @Override
-            public void run() {
-                try {
-                    IUpdateClientList(clients);
-                    sendStatusChangedMessage(STATUS_NORMAL);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    sendStatusChangedMessage(STATUS_ERROR);
-                }
-            }
-        };
     }
 
     @Override
@@ -102,33 +57,13 @@ public class SoftApManageService extends Service implements WifiManager.SoftApCa
             BLOCKED_MAC_ADDRESS_FILE = new File(getDataDir(), "blocked.txt");
         }
         if (mHandler == null) {
-            mHandler = new Handler(getMainLooper()) {
-                @Override
-                public void handleMessage(@NonNull Message msg) {
-                    super.handleMessage(msg);
-                    if (msg.what == MSG_STATUS_CHANGED) {
-                        for (StatusListener listener : mListeners) {
-                            listener.onStatusChanged(msg.arg1);
-                        }
-                    } else if (msg.what == MSG_UPDATE_CLIENT_LIST) {
-                        if (mClientUpdateThread != null &&
-                                mClientUpdateThread.getState() != Thread.State.NEW &&
-                                mClientUpdateThread.getState() != Thread.State.TERMINATED) {
-                            mHandler.removeMessages(MSG_UPDATE_CLIENT_LIST);
-                            mHandler.sendMessageDelayed(msg, 1000);
-                            return;
-                        }
-                        mClientUpdateThread = createClientUpdateThread((List<WifiClient>) msg.obj);
-                        mClientUpdateThread.start();
-                    }
-                }
-            };
+            mHandler = new Handler(Looper.myLooper());
         }
         if (mNetworkManagementService == null) {
             mNetworkManagementService = INetworkManagementService.Stub.asInterface(ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE));
         }
-        if (mWifiManager == null) {
-            mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (mTetheringManager == null) {
+            mTetheringManager = (TetheringManager) getSystemService(TetheringManager.class);
         }
         if (mBlockedMACList.isEmpty()) {
             updateBlockedMACList();
@@ -138,15 +73,18 @@ public class SoftApManageService extends Service implements WifiManager.SoftApCa
             MACDataHelper.init(this);
         }
 
-        registerReceiver(mApStatusReceiver, new IntentFilter(WifiManager.WIFI_AP_STATE_CHANGED_ACTION));
+        try {
+            mTetheringManager.registerTetheringEventCallback(new HandlerExecutor(mHandler), this);
+        } catch (Exception e) {
+            // do nothing
+        }
 
         return ret;
     }
 
     @Override
     public void onDestroy() {
-        unregisterReceiver(mApStatusReceiver);
-        mWifiManager.unregisterSoftApCallback(this);
+        mTetheringManager.unregisterTetheringEventCallback(this);
         super.onDestroy();
     }
 
@@ -215,35 +153,33 @@ public class SoftApManageService extends Service implements WifiManager.SoftApCa
         return false;
     }
 
-    private void IUpdateClientList(List<WifiClient> clients) {
-
+    public void onClientsChanged(Collection<TetheredClient> clients) {
         HashMap<String, ClientInfo> clientInfoHashMap = new HashMap<>();
-
         sendStatusChangedMessage(STATUS_CLIENTS_REFRESHING);
         // 取得当前连接的设备
-        for (WifiClient client : clients) {
+        for (TetheredClient client : clients) {
+            if (client.getTetheringType() != TetheringManager.TETHERING_WIFI) {
+                continue;
+            }
             String macAddress = client.getMacAddress().toString();
             ClientInfo info = new ClientInfo(macAddress);
             MACData macData = MACDataHelper.findMACData(macAddress);
             if (macData != null) {
                 info.setManufacturer(macData.toString());
             }
-            clientInfoHashMap.put(macAddress, info);
-        }
-        // MAC-IP 匹配
-        String res = execCommand(String.format(IP_NEIGHBOR_COMMAND_FORMAT, mInterfaceName));
-        if (!TextUtils.isEmpty(res)) {
-            String[] lines = res.split("\n");
-            for (String line : lines) {
-                String[] values = line.split(" ");
-                String ipAddress = values[COLUMN_IP_ADDRESS];
-                String macAddress = values[COLUMN_MAC_ADDRESS];
-                if (!isValidMACAddress(macAddress)) continue;
-                if (clientInfoHashMap.containsKey(macAddress)) {
-                    ClientInfo info = clientInfoHashMap.get(macAddress);
-                    info.addIPAddress(ipAddress);
+            for (TetheredClient.AddressInfo addressInfo : client.getAddresses()) {
+                info.addIPAddress(addressInfo.getAddress().getAddress().getHostAddress());
+                if (!TextUtils.isEmpty(addressInfo.getHostname())) {
+                    info.setName(addressInfo.getHostname());
                 }
             }
+            if (info.getIPAddressArray().length == 0) {
+                continue;
+            }
+            if (TextUtils.isEmpty(info.getName())) {
+                info.setName(info.getIPAddressArray()[0]);
+            }
+            clientInfoHashMap.put(macAddress, info);
         }
 
         // 取得被屏蔽的设备列表
@@ -261,86 +197,21 @@ public class SoftApManageService extends Service implements WifiManager.SoftApCa
             }
         }
 
-        for (ClientInfo clientInfo : clientInfoHashMap.values()) {
-            getClientName(clientInfo);
-        }
-
         mClients.clear();
         mClients.putAll(clientInfoHashMap);
 
         sendStatusChangedMessage(STATUS_CLIENTS_REFRESH_DONE);
     }
 
-    private boolean isValidMACAddress(String mac) {
-        return mac != null && mac.matches("([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}");
-    }
-
-    private boolean isIPV4Address(String ip) {
-        return ip != null && ip.matches("((25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))");
-    }
-
     private void sendStatusChangedMessage(int what) {
-        Message msg = mHandler.obtainMessage();
-        msg.what = MSG_STATUS_CHANGED;
-        msg.arg1 = what;
-        mHandler.sendMessage(msg);
-    }
-
-    private void getClientName(ClientInfo client) {
-        String[] ips = client.getIPAddressArray();
-        if (ips == null || ips.length == 0) return;
-        try {
-            boolean isIPV4 = isIPV4Address(ips[0]);
-            String[] ipStr;
-            if (isIPV4) {
-                ipStr = ips[0].split("\\.");
-            } else {
-                ipStr = ips[0].split(":");
-            }
-            byte[] ipBuffer = new byte[ipStr.length];
-            for(int i = 0; i < ipStr.length; i++){
-                ipBuffer[i] = (byte)(Integer.parseInt(ipStr[i], isIPV4 ? 10 : 16) & 0xff);
-            }
-            InetAddress inetAddress = InetAddress.getByAddress(ipBuffer);
-            client.setName(inetAddress.getHostName());
-        } catch (UnknownHostException e) {
-            client.setName(ips[0]);
+        for (StatusListener listener : mListeners) {
+            listener.onStatusChanged(what);
         }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return new SoftApManageBinder();
-    }
-
-    @Override
-    public void onConnectedClientsChanged(List<WifiClient> clients) {
-        Message msg = mHandler.obtainMessage();
-        msg.what = MSG_UPDATE_CLIENT_LIST;
-        msg.obj = clients;
-        mHandler.sendMessage(msg);
-    }
-
-    private static String execCommand(String cmd) {
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            Process process = runtime.exec(cmd);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StringBuffer stringBuffer = new StringBuffer();
-            char[] buff = new char[1024];
-            int ch = 0;
-            while ((ch = reader.read(buff)) != -1) {
-                stringBuffer.append(buff, 0, ch);
-            }
-            reader.close();
-            process.waitFor();
-            if (process.exitValue() == 0) {
-                return stringBuffer.toString();
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public class SoftApManageBinder extends Binder {
